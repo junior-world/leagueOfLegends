bf94ec4b4203f188241c36dcdf69305d6a4c3ca1
limseonghyun <sksrpf1126@naver.com>
2021-08-13 18:18:10 +0900

    main import 문제 해결

--- src/page/main.jsx
+++ src/page/main.jsx
@@ -1,8 +1,7 @@
 import { Switch, Route } from 'react-router-dom';
 import NavBar from '../components/main/NavBar';
-import MainPage from '../components/main/MainPage';
 
 const Main = () => {
-  return <NavBar></NavBar>;
+    return <NavBar></NavBar>;
 };
 export default Main;
