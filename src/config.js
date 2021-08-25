89e86f39677e6a5e7f46bdc95b23b01e20d990b8
limseonghyun <sksrpf1126@naver.com>
2021-08-25 23:23:11 +0900

    master 간 충돌 해결

--- src/config.js
+++ src/config.js
@@ -12,54 +12,47 @@ export const CHAMPIONS_DATA =
   'http://ddragon.leagueoflegends.com/cdn/11.16.1/data/ko_KR/champion.json'; //모든 챔피언 정보
 
 //http://ddragon.leagueoflegends.com/cdn/11.6.1/data/ko_KR/champion/<champion_name>.json
-export const CHAMPION_DATA ='http://ddragon.leagueoflegends.com/cdn/11.6.1/data/ko_KR/champion/.json'; //특정 챔피언 정보
+export const CHAMPION_DATA =
+  'http://ddragon.leagueoflegends.com/cdn/11.6.1/data/ko_KR/champion/.json'; //특정 챔피언 정보
 
-
-
+export const CHAMPION_ICON_IMG =
+  'https://ddragon.leagueoflegends.com/cdn/11.16.1/img/champion/';
 
 // (소환사 id , 닉네임) 등등 기본정보 url
-export const SUMMONER_URL=`/lol/summoner/v4/summoners/by-name/`;
+export const SUMMONER_URL = `/lol/summoner/v4/summoners/by-name/`;
 
-// (소환사 티어 ,승 ,패 ,포인트) 등의 정보 url 
-export const ENTRIES_URL =`/lol/league/v4/entries/by-summoner/`;
+// (소환사 티어 ,승 ,패 ,포인트) 등의 정보 url
+export const ENTRIES_URL = `/lol/league/v4/entries/by-summoner/`;
 
-
-// ?endIndex=19 20 가져오기  에서 matchId를 가져오고 
-// 소환사 최근 게임 가져오기  
+// ?endIndex=19 20 가져오기  에서 matchId를 가져오고
+// 소환사 최근 게임 가져오기
 // https://asia.api.riotgames.com/lol/match/v5/matches/by-puuid/{puuid}/ids?start=0&count=20
 // 여긴 match-V5
-// export const MATCHLISTS_URL = `/lol/match/v5/matches/by-puuid/` 
- 
+// export const MATCHLISTS_URL = `/lol/match/v5/matches/by-puuid/`
+
 ///lol/match/v4/matchlists/by-account/1tt6BqbDr6XchhkNBASZCvqj7LCPUljgTGCjjxqxMUAo?endIndex=19&beginIndex=0&api_key=
 export const MATCHLISTS_URL = `/lol/match/v4/matchlists/by-account/`;
 
-
-// 해당게임에 대한 내용(아이템,팀원 승, 패 등등) 가져오기 
+// 해당게임에 대한 내용(아이템,팀원 승, 패 등등) 가져오기
 // https://kr.api.riotgames.com/lol/match/v4/matches/{matchId}
-export const MATCHS_URL =`/lol/match/v4/matches/` 
+export const MATCHS_URL = `/lol/match/v4/matches/`;
 
-
-// 모든 스펠 정보  
-export const SPELL_DATA = `http://ddragon.leagueoflegends.com/cdn/11.16.1/data/en_US/summoner.json` 
+// 모든 스펠 정보
+export const SPELL_DATA = `http://ddragon.leagueoflegends.com/cdn/11.16.1/data/en_US/summoner.json`;
 // http://ddragon.leagueoflegends.com/cdn/11.16.1/img/spell/<spell_name>.png
 
-
-
-
 /***
-    ***   모든 룬 정보들
-    ***  - 메인 룬 이미지 (format + example)
-    ***   https://ddragon.leagueoflegends.com/cdn/img/perk-images/Styles/<style_name>.png
-    ***
-    ***   https://ddragon.leagueoflegends.com/cdn/img/perk-images/Styles/7200_Domination.png
-**/
+ ***   모든 룬 정보들
+ ***  - 메인 룬 이미지 (format + example)
+ ***   https://ddragon.leagueoflegends.com/cdn/img/perk-images/Styles/<style_name>.png
+ ***
+ ***   https://ddragon.leagueoflegends.com/cdn/img/perk-images/Styles/7200_Domination.png
+ **/
 export const RUNS_DATA = `https://ddragon.leagueoflegends.com/cdn/10.6.1/data/en_US/runesReforged.json`;
-
-
 
 // https://kr.api.riotgames.com/lol/champion-mastery/v4/champion-masteries/by-summoner/{encryptedSummonerId}?api_key={}
 export const CHAMPION_MASTERIES = `/lol/champion-mastery/v4/champion-masteries/by-summoner/`;
 
-  //숙련도 레벨별 문양 , 배너
+//숙련도 레벨별 문양 , 배너
 // https://raw.communitydragon.org/latest/plugins/rcp-fe-lol-profiles/global/default/mastery_level2.png
 // https://raw.communitydragon.org/latest/plugins/rcp-fe-lol-profiles/global/default/mastery_level2banner.png
--- src/page/champInfo.jsx
+++ src/page/champInfo.jsx
@@ -1,12 +1,8 @@
 import React, { useEffect } from 'react';
-<<<<<<< Updated upstream
 import styled from 'styled-components';
 import NavBar from '../components/main/NavBar';
 import ChampRotation from '../components/champ_info/rotation_info/champRotation';
 import ChampList from '../components/champ_info/position_info/champList';
-=======
-import Rotation from '../controller/rotation';
->>>>>>> Stashed changes
 
 const MainContents = styled.div`
   width: 100%;
