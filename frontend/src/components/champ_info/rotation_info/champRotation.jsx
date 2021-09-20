1d406ce87c556d302fd040a0aa7f2eba7b3fed23
limseonghyun <sksrpf1126@naver.com>
2021-09-20 17:57:02 +0900

    mysql -> maria DB swap
    rotationAPI 이진 검색 알고리즘 에러 해결

--- frontend/src/components/champ_info/rotation_info/champRotation.jsx
+++ frontend/src/components/champ_info/rotation_info/champRotation.jsx
@@ -1,90 +1,81 @@
-import React, { useEffect, useState } from 'react';
-import styled from 'styled-components';
-import DefaultHeader from '../defaultHeader';
-import GetRotationData from '../../../controller/champ_info/getRotationData';
-import ChampImg from '../champImg';
-import * as config from '../../../config';
-import ChampName from '../champName';
-import { Link } from 'react-router-dom';
-import BackendTest from '../../../controller/champ_info/backendTest';
+import React, { useEffect, useState } from 'react'
+import styled from 'styled-components'
+import DefaultHeader from '../defaultHeader'
+import GetRotationData from '../../../controller/champ_info/getRotationData'
+import ChampImg from '../champImg'
+import * as config from '../../../config'
+import ChampName from '../champName'
+import { Link } from 'react-router-dom'
+import BackendTest from '../../../controller/champ_info/backendTest'
 
 const RotationSection = styled.div`
-    display: flex;
-    width: 1200px;
-    border: solid 1px #e9eff4;
-    margin-top: 80px;
-    flex-direction: column;
-`;
+  display: flex;
+  width: 1200px;
+  border: solid 1px #e9eff4;
+  margin-top: 80px;
+  flex-direction: column;
+`
 
 const RotationMain = styled.div`
-    display: flex;
-    flex-wrap: wrap;
-    padding: 10px;
-`;
+  display: flex;
+  flex-wrap: wrap;
+  padding: 10px;
+`
 
 const ChampItem = styled.div`
-    width: 90px;
-    height: 110px;
-    white-space: nowrap;
-    display: flex;
-    flex-direction: column;
-`;
+  width: 90px;
+  height: 110px;
+  white-space: nowrap;
+  display: flex;
+  flex-direction: column;
+`
 
 const HLink = styled(Link)`
-    text-decoration: none;
-`;
+  text-decoration: none;
+`
 
 function ChampRotation(props) {
-    const [isRotationLoading, setIsRotationLoading] = useState(false);
-    const [rotationData, setRotationData] = useState([]);
+  const [isRotationLoading, setIsRotationLoading] = useState(false)
+  const [rotationData, setRotationData] = useState([])
 
-    useEffect(() => {
-        BackendTest().then((res) => {
-            console.log(res);
-        });
-        GetRotationData().then((res) => {
-            setRotationData(res);
-            setIsRotationLoading(true);
-        });
-    }, []);
+  useEffect(() => {
+    GetRotationData().then((res) => {
+      setRotationData(res)
+      setIsRotationLoading(true)
+    })
+  }, [])
 
-    return (
-        <RotationSection>
-            <DefaultHeader headerTitle={'- 금주의 로테이션 챔피언'} />
-            <RotationMain>
-                {isRotationLoading &&
-                    rotationData.map((rotationObject, i) => {
-                        return (
-                            <HLink
-                                key={rotationObject.id + rotationObject.name}
-                                to={{
-                                    pathname:
-                                        '/championInfo/' + rotationObject.id,
-                                    state: { championId: rotationObject.id },
-                                }}>
-                                <ChampItem
-                                    key={
-                                        rotationObject.id + rotationObject.name
-                                    }>
-                                    <ChampImg
-                                        key={rotationObject.id}
-                                        imagePath={
-                                            config.CHAMPION_ICON_IMG +
-                                            rotationObject.id +
-                                            '.png'
-                                        }
-                                    />
-                                    <ChampName
-                                        key={rotationObject.name} //lol 챔피언 이름은 중복 가능성이 없으므로 key로 사용
-                                        champName={rotationObject.name}
-                                    />
-                                </ChampItem>
-                            </HLink>
-                        );
-                    })}
-            </RotationMain>
-        </RotationSection>
-    );
+  return (
+    <RotationSection>
+      <DefaultHeader headerTitle={'- 금주의 로테이션 챔피언'} />
+      <RotationMain>
+        {isRotationLoading &&
+          rotationData.map((rotationObject, i) => {
+            return (
+              <HLink
+                key={rotationObject.id + rotationObject.name}
+                to={{
+                  pathname: '/championInfo/' + rotationObject.id,
+                  state: { championId: rotationObject.id },
+                }}>
+                <ChampItem key={rotationObject.id + rotationObject.name}>
+                  <ChampImg
+                    key={rotationObject.id}
+                    imagePath={
+                      config.CHAMPION_ICON_IMG + rotationObject.id + '.png'
+                    }
+                  />
+                  <ChampName
+                    key={rotationObject.name} //lol 챔피언 이름은 중복 가능성이 없으므로 key로 사용
+                    champName={rotationObject.name}
+                  />
+                </ChampItem>
+              </HLink>
+            )
+          })}
+      </RotationMain>
+    </RotationSection>
+  )
 }
 
-export default ChampRotation;
+export default ChampRotation
--- frontend/src/controller/champ_info/getAllChampData.js
+++ frontend/src/controller/champ_info/getAllChampData.js
@@ -1,37 +1,32 @@
-import axios from 'axios';
-import * as config from '../../config';
-import * as Hangul from 'hangul-js';
+import axios from 'axios'
+import * as config from '../../config'
+import * as Hangul from 'hangul-js'
 /**
  * 모든 챔피언의 기본 정보가 담겨있는 Json파일을 요청
  * 초성검색을 위하여 외부라이브러리 사용. for문을 통해 초성 추출 후 객체에 재할당
  * @returns 챔피언의 모든 정보를 data라는 변수로 반환 + 가공을 통해 초성 객체 추가(객체명 -> diassembled)
  */
 const GetAllChampData = async () => {
-    const {
-        data: { data },
-    } = await axios.get(config.CHAMPIONS_DATA).catch((err) => {
-        if (400 <= err.response.status && err.response.status < 500) {
-            console.error(
-                `-----${err.response.status}번 에러 -> request 문제-----`,
-            );
-        } else if (500 <= err.response.status && err.response.status < 600) {
-            console.error(
-                `-----${err.response.status}번 에러 -> server 문제-----`,
-            );
-        }
-    });
-
-    //객체에 name에서 초성 추출 후 disassembled 객체를 만들어 data 객체에 재할당
-    for (const key in data) {
-        var dis = Hangul.disassemble(data[key].name, true);
-        var cho = dis.reduce((prev, elem) => {
-            elem = elem[0] ? elem[0] : elem;
-            return prev + elem;
-        }, '');
-        data[key].diassembled = cho;
+  const {
+    data: { data },
+  } = await axios.get(config.CHAMPIONS_DATA).catch((err) => {
+    if (400 <= err.response.status && err.response.status < 500) {
+      console.error(`-----${err.response.status}번 에러 -> request 문제-----`)
+    } else if (500 <= err.response.status && err.response.status < 600) {
+      console.error(`-----${err.response.status}번 에러 -> server 문제-----`)
     }
+  })
 
-    return data;
-};
+  //객체에 name에서 초성 추출 후 disassembled 객체를 만들어 data 객체에 재할당
+  for (const key in data) {
+    var dis = Hangul.disassemble(data[key].name, true)
+    var cho = dis.reduce((prev, elem) => {
+      elem = elem[0] ? elem[0] : elem
+      return prev + elem
+    }, '')
+    data[key].diassembled = cho
+  }
+  return data
+}
 
-export default GetAllChampData;
+export default GetAllChampData
